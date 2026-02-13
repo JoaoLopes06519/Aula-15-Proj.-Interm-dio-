b5be369631d0a5fee8791fe8bbb58f3a383645e8
JoaoLopes06519 <shark06519@sharkcoders.pt>
2026-02-13 20:01:05 +0000

    Mecanicas.py

--- mecanicas.py
+++ mecanicas.py
@@ -1,18 +1,16 @@
-precovendadacia = 20000
-precovendapeugeot = 35000
 precostockdacia = 15000
 precostockpeugeot = 40000
 saldoatualCLIENTE = 40000
 saldoatualSTAND = 5000
 stockDACIASANDERO = 30
 stockPEUGEOT3008 = 15
-
+stockPNEUSCONTINENTAL = 40
 
 def comprarCarro(saldoCliente, custoTotal):
     saldoCliente -= custoTotal
     return saldoCliente
 def gerir_stock(saldoatualSTAND):
-    escolha = int(input("Queres encher o stock do 1- DACIA SANDERO ou do 2- PEUGEOT3008?"))
+    escolha = int(input("Queres encher o stock do 1- DACIA SANDERO, do 2- PEUGEOT3008 ou do 3- PNEUS CONTINENTAL?"))
     if escolha == 1:
         verificaçãostockdacia = int(input("Quanto dinheiro é que tu tens?"))
         if verificaçãostockdacia <15000:
@@ -32,3 +30,16 @@ def gerir_stock(saldoatualSTAND):
              print("Podes comprar")
              saldoatualSTAND = saldoatualSTAND - 15000
              print(saldoatualSTAND)
+
+    if escolha == 3:
+        verificaçãostockpneuscontinental = int(input("Quanto dinheiro é que tu tens?"))
+        if verificaçãostockpneuscontinental == "Não":
+            print("Não tens dinheiro suficiente")
+            return escolha
+        if verificaçãostockpneuscontinental >= 15000:
+            print("Podes comprar")
+            saldoatualSTAND = saldoatualSTAND - 15000
+            print(saldoatualSTAND)
+
+
+
