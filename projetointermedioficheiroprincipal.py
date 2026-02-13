b10f8ddd01d7d18024f179a6cb83664658ee9e3f
JoaoLopes06519 <shark06519@sharkcoders.pt>
2026-02-13 20:02:09 +0000

    projetointermedioficheiroprincipal.py

--- projetointermedioficheiroprincipal.py
+++ projetointermedioficheiroprincipal.py
@@ -1,16 +1,18 @@
 import time
 import mecanicas
-from mecanicas import saldoatualCLIENTE
+from mecanicas import saldoatualCLIENTE, saldoatualSTAND
 
 precodacia = 20000
 precopeugeot = 35000
+precocontinental = 50
+listaitens = []
 
 print("BEM-VINDO AO STAND AUTOMÓVEL")
-def escolha_menu():
-     escolha = int(input("Quais destas opções preferes, 1-DACIA SANDERO, 2- PEUGEOT 3008, 3- STOCK, 4- SAIR"))
-     time.sleep(1)
-     while escolha != 4:
-        time.sleep(1)
+def escolha_menu(saldoatualSTAND):
+     escolha = int(input("Quais destas opções preferes, 1-DACIA SANDERO, 2- PEUGEOT 3008, 3- PNEUS CONTINENTAL, 4- STOCK, 5- SAIR"))
+     time.sleep(0.5)
+     while escolha != 5:
+        time.sleep(0.5)
         if escolha == 1:
             quantdacias = int(input("Quantos dacias queres?"))
             totalapagardacia = quantdacias * precodacia
@@ -22,7 +24,7 @@ def escolha_menu():
             if totalapagardacia <= mecanicas.saldoatualCLIENTE:
                 mecanicas.saldoatualCLIENTE= mecanicas.comprarCarro(mecanicas.saldoatualCLIENTE, totalapagardacia)
                 print(f"Total a pagar:{totalapagardacia}\nSaldo atual:{mecanicas.saldoatualCLIENTE}")
-
+                listaitens.add("Dacia Sandero")
 
 
             mecanicas.saldoatualSTAND += totalapagardacia
@@ -31,7 +33,7 @@ def escolha_menu():
         if escolha == 2:
           quantpeugeot = int(input("Quantos Peugeot queres?"))
           totalapagarpeugeot = quantpeugeot * precopeugeot
-          time.sleep(1)
+          time.sleep(0.5)
           if quantpeugeot > 30:
               print("Não temos Peugeot suficientes")
 
@@ -40,15 +42,36 @@ def escolha_menu():
                   print("Não tens dinheiro suficiente")
               if totalapagarpeugeot <= 40000:
                   print(f"Total a pagar:{totalapagarpeugeot}")
-                  mecanicas.saldoatualCLIENTE = mecanicas.comprarCarro(mecanicas.saldoatualCLIENTE, totalapagardacia)
-                  print(f"Total a pagar:{totalapagardacia}\nSaldo atual:{mecanicas.saldoatualCLIENTE}")
+                  mecanicas.saldoatualCLIENTE = mecanicas.comprarCarro(mecanicas.saldoatualCLIENTE, totalapagarpeugeot)
+                  print(f"Total a pagar:{totalapagarpeugeot}\nSaldo atual:{mecanicas.saldoatualCLIENTE}")
+                  listaitens.add("Peugeot 3008")
 
               mecanicas.saldoatualSTAND += totalapagarpeugeot
               mecanicas.saldoatualCLIENTE -= totalapagarpeugeot
 
         if escolha == 3:
-           mecanicas.gerir_stock()
+          quantpneuscontinental = int(input("Quantos pneus Continental queres?"))
+          totalapagarpneuscontinental = quantpneuscontinental * precocontinental
+          time.sleep(0.5)
+          if quantpneuscontinental > 30:
+              print("Não temos pneus suficientes")
 
-        escolha = int(input("Quais destas opções preferes, 1- DACIA SANDERO, 2- PEUGEOT 3008, 3- STOCK, 4- SAIR"))
+          if quantpneuscontinental <= 30:
+              if totalapagarpneuscontinental > 40000:
+                  print("Não tens dinheiro suficiente")
+              if totalapagarpneuscontinental <= 40000:
+                  print(f"Total a pagar:{totalapagarpneuscontinental}")
+                  mecanicas.saldoatualCLIENTE = mecanicas.comprarCarro(mecanicas.saldoatualCLIENTE, totalapagarpneuscontinental)
+                  print(f"Total a pagar:{totalapagarpneuscontinental}\nSaldo atual:{mecanicas.saldoatualCLIENTE}")
+                  listaitens.add("Pneu Continental")
 
-escolha_menu()
+              mecanicas.saldoatualSTAND += totalapagarpneuscontinental
+              mecanicas.saldoatualCLIENTE -= totalapagarpneuscontinental
+
+        if escolha == 4:
+           mecanicas.gerir_stock(saldoatualSTAND)
+
+        escolha = int(input("Quais destas opções preferes, 1- DACIA SANDERO, 2- PEUGEOT 3008, 3- PNEUS CONTINENTAL 4- STOCK, 5- SAIR"))
+
+escolha_menu(saldoatualSTAND)
+
